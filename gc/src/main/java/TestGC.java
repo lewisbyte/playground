ed27c4ffd2ede700578756e68d09bae925f33564
lewis <lingyidx@gmail.com>
2020-12-09 17:20:07 +0800

    添加GC 相关文档

---
+++ gc/src/main/java/TestGC.java
@@ -0,0 +1,34 @@
+public class TestGC {
+
+    // 1MB 空间大小
+    public static final int _1MB = 1 << 20;
+
+
+    public static void main(String[] args) {
+        testAllocation();
+    }
+
+    /**
+     * jvm 参数：-verbose:gc  -Xms20M -Xmx20M -Xmn10M -XX:+PrintGCDetails
+     */
+    public static void testAllocation() {
+        byte a1[], a2[], a3[], a4[];
+
+        a1 = new byte[2 * _1MB];
+        a2 = new byte[2 * _1MB];
+        a3 = new byte[2 * _1MB];
+        a4 = new byte[4 * _1MB];
+        a4 = new byte[4 * _1MB];
+    }
+
+
+    /**
+     * jvm 参数：-verbose:gc  -Xms20M -Xmx20M -Xmn10M -XX:+PrintGCDetails -XX:PretenureSizeThreshold
+     */
+    public static void testAllocation2() {
+        byte a4[];
+        a4 = new byte[4 * _1MB];
+    }
+
+
+}
